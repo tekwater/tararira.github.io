011d39e9d40f8e771f4d706c276141d0da63b8ad
たらりら <tararira03@gmail.com>
2020-07-24 03:51:00 +0900

    add counter

---
+++ works/counter.js
@@ -0,0 +1,10 @@
+let count = 0
+const countUp = () => {
+  count++
+  const countElement = document.querySelector('#count')
+  countElement.innerText = '回数: ' + count
+
+  const reset = () => {
+  countElement.innerText = '回数: ' + count
+  }
+}
