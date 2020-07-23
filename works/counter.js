e59bb9685ce8c753ec85a5d8d2042dceb71f3b5d
たらりら <tararira03@gmail.com>
2020-07-24 04:54:43 +0900

    fix

--- works/counter.js
+++ works/counter.js
@@ -1,16 +1,19 @@
 let count = 0
+
 const countUp = () => {
-  count++
-  const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+        count++
+        const countElement = document.querySelector('#count')
+        countElement.innerText = '回数: ' + count
 }
+
 const countDown = () => {
-  count--
-  const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+        count--
+        const countElement = document.querySelector('#count')
+        countElement.innerText = '回数: ' + count
 }
+
 const reset = () => {
-  count = 0
-  const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+        count = 0
+        const countElement = document.querySelector('#count')
+        countElement.innerText = '回数: ' + count
 }
